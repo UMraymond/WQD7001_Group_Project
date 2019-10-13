ffe55ad8aa2047d2a67d736089117a2f568cf404
UMraymond <55731716+UMraymond@users.noreply.github.com>
2019-10-13 21:42:42 +0800

    Create server.R

---
+++ server.R
@@ -0,0 +1,42 @@
+## server.R
+
+library(shiny)
+library(tidyverse) ; library(httr) ; library(jsonlite); library(glue)
+require("httr")
+
+shinyServer(function(input, output){
+  output$currencyPlot <- renderPlot({
+    
+    
+    year = "2019"
+    currency_code = input$chosen
+    month = "3"
+    session = c("0900", "1130", "1200", "1700")
+    
+    
+    path = paste0("https://api.bnm.gov.my/public/exchange-rate/", currency_code, "/", "year/",  year, "/", "month/", month)
+    
+    response = GET(path, session = session, 
+                   accept("application/vnd.BNM.API.v1+json"),
+                   user_agent("httr"))
+    
+    response <- content(response, as = "text", encoding = "UTF-8")
+    #response
+    
+    df <- fromJSON(response, flatten = TRUE) %>% 
+      data.frame()
+    
+    df <- select(df,
+                 date = data.rate.date, 
+                 currency = data.currency_code, 
+                 buying = data.rate.buying_rate,
+                 selling = data.rate.selling_rate)
+    
+    df <- df[order(df$date),]
+    #head(df)
+    
+    ggplot(df, aes(x=date, y=selling, group =1)) + geom_line() + geom_point() + geom_smooth(method='lm',formula=y~x) + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle(paste(as.character(df$currency[1]), "/MYR currency", sep = "")) +
+      theme(plot.title = element_text(hjust = 0.5))
+    
+  }, height = 500, width = 800)
+})
