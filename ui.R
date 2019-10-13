ea9c7664830c67fb08dfbc3aafa5de05d0ff31ee
UMraymond <55731716+UMraymond@users.noreply.github.com>
2019-10-13 21:42:13 +0800

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,22 @@
+##ui.R
+
+
+library(shiny)
+library(tidyverse) ; library(httr) ; library(jsonlite); library(glue)
+require("httr")
+shinyUI(fluidPage(
+  titlePanel("Hello Shiny BNM API!"),
+  sidebarLayout(
+    sidebarPanel(
+      
+      selectInput("chosen", label = "Currency Choice", choices = c("USD", "EUR"), selected = "USD", multiple = FALSE),
+      br(),
+      p(strong("Data Set Info:"), "From", code("BNM API"), "online"),
+      p("Past 30 days of selected currency with MYR currency.")
+    ),
+    mainPanel(
+      plotOutput("currencyPlot")
+    )
+  )
+  
+))
