401756008bceee90a0f718867ec79503556b57c8
Chong-KinHow <55740788+Chong-KinHow@users.noreply.github.com>
2019-10-17 15:08:59 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -2,20 +2,20 @@
 
 
 library(shiny)
-library(tidyverse) ; library(httr) ; library(jsonlite); library(glue)
+library(tidyverse) ; library(httr) ; library(jsonlite); library(glue); library(ggplot2); library(plotly)
 require("httr")
 shinyUI(fluidPage(
   titlePanel("Hello Shiny BNM API!"),
   sidebarLayout(
     sidebarPanel(
       
-      selectInput("chosen", label = "Currency Choice", choices = c("USD", "EUR"), selected = "USD", multiple = FALSE),
+      selectInput("chosen", label = "Currency Choice", choices = c("USD", "EUR", "SGD", "AUD", "GBP", "EUR", "JPY"), selected = "USD", multiple = FALSE),
       br(),
       p(strong("Data Set Info:"), "From", code("BNM API"), "online"),
       p("Past 30 days of selected currency with MYR currency.")
     ),
     mainPanel(
-      plotOutput("currencyPlot")
+      plotlyOutput("currencyPlot")
     )
   )
   
