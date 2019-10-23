fb034f902032765b966ab342c8c08dc3a8ef04d5
jou-hui <55745355+jou-hui@users.noreply.github.com>
2019-10-23 21:59:05 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,18 +1,19 @@
 ##ui.R
 
 
-library(shiny)
+library(shiny) ; library(shinythemes)
 library(tidyverse) ; library(httr) ; library(jsonlite); library(glue); library(ggplot2); library(plotly)
 require("httr")
 shinyUI(fluidPage(
-  titlePanel("Hello Shiny BNM API!"),
+  theme = shinytheme("darkly"),
+  titlePanel(h1("Hello Shiny BNM API!")),
   sidebarLayout(
     sidebarPanel(
       
-      selectInput("chosen", label = "Currency Choice", choices = c("USD", "EUR", "SGD", "AUD", "GBP", "EUR", "JPY"), selected = "USD", multiple = FALSE),
+      selectInput("chosen", label = h3("Currency Choice"), choices = c("USD", "EUR", "SGD", "AUD", "GBP", "EUR", "JPY"), selected = "USD", multiple = FALSE),
       radioButtons("type", label = "Buying/Selling", choices = c("Buying", "Selling"), selected = "Buying"),
       br(),
-      p(strong("Data Set Info:"), "From", code("BNM API"), "online"),
+      p(strong("Data Set Info:"), "From", span("BNM API", style = "color:red"), "online"),
       p("Past 30 days of selected currency with MYR currency.")
     ),
     mainPanel(
