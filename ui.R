d8a94caa00b884eae6c09c408c4c269b8e13bbdb
jou-hui <55745355+jou-hui@users.noreply.github.com>
2019-10-27 15:02:33 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,7 +1,6 @@
 ##ui.R
 
-
-library(shiny) ; library(shinythemes)
+library(shiny); library(shinythemes)
 library(tidyverse) ; library(httr) ; library(jsonlite); library(glue); library(ggplot2); library(plotly)
 require("httr")
 shinyUI(fluidPage(
@@ -12,12 +11,26 @@ shinyUI(fluidPage(
       
       selectInput("chosen", label = h3("Currency Choice"), choices = c("USD", "EUR", "SGD", "AUD", "GBP", "EUR", "JPY"), selected = "USD", multiple = FALSE),
       radioButtons("type", label = "Buying/Selling", choices = c("Buying", "Selling"), selected = "Buying"),
+      p("Predict the next 10 days price"),
+      actionButton("predict", label = "Predict"),
+      br(),
       br(),
       p(strong("Data Set Info:"), "From", span("BNM API", style = "color:red"), "online"),
-      p("Past 30 days of selected currency with MYR currency.")
+      p("Past 30 days of selected currency with MYR currency."),
+      br(),
+      h3("Check the gold price"),
+      actionButton("check", label = "Check"),
+      br(),
+      br(),
+      radioButtons("gold", label = "Buying/Selling", choices = c("Buying", "Selling"), selected = "Buying"),
+      radioButtons("gram", label = "Choose units", choices = c("Ounce", "Gram"), selected = "Ounce"),
+      br(),
+      br()
     ),
     mainPanel(
-      plotlyOutput("currencyPlot")
+      plotlyOutput("currencyPlot"),
+      br(),
+      plotlyOutput("goldplot")
     )
   )
   
